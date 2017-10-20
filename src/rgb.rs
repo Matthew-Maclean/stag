0cbbb009513b1c5d41e3c6f0e450846c23babc50
Matthew M <myself123@live.com>
2017-10-20 11:42:46 -0400

    Added test for Rgb codec

--- src/rgb.rs
+++ src/rgb.rs
@@ -184,3 +184,41 @@ impl FromStr for RgbMode
         }
     }
 }
+
+#[cfg(test)]
+mod test
+{
+    use image::{ImageBuffer, Rgb};
+    use rand::StdRng;
+    
+    use codec::Codec;
+    use super::*;
+
+    #[test]
+    fn all()
+    {
+        let mut image = ImageBuffer::from_pixel(
+            30,
+            8,
+            Rgb
+            {
+                data: [127u8; 3],
+            });
+
+        let payload = vec![
+            1,2, 3, 4, 5, 6, 7, 8, 9, 10,
+            11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+            21, 22, 23, 24, 25, 26, 27, 28, 29, 30
+        ];
+
+        let mut buf = vec![0; 30];
+
+        let rng = StdRng::new().unwrap();
+
+        RgbCodec::encode(&mut image, &payload, RgbMode::All, rng);
+
+        RgbCodec::decode(&image, &mut buf, 30, RgbMode::All);
+
+        assert_eq!(payload, buf);
+    }
+}
